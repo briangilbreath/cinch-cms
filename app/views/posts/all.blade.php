30fce0ffef8377348a4ab45bab95de5d625791bb
briangilbreath <bgilbreath87@gmail.com>
2014-12-21 02:21:32 -0500

    reduced first page paragraph

--- app/views/posts/all.blade.php
+++ app/views/posts/all.blade.php
@@ -10,7 +10,7 @@
 
 				<h2>{{link_to($single_post->slug, $single_post->title)}}</h2>
 				<h5 class="date">{{$single_post->created_at}}</h5>
-				<p> {{Str::words($single_post->body,$words = 50, $end='...');}}</p>
+				<p> {{Str::words($single_post->body,$words = 40, $end='...');}}</p>
 				@if($single_post->tags)
 					@if($single_post->tags->first())
 						<span class="tags">
